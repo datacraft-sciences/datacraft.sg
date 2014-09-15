0a5b1f37a877ece4593b2f364900d47539801448
mikera <mike.r.anderson.13@gmail.com>
2014-09-15 10:08:16 +0800

    add core namespace

---
+++ src/main/clj/datacraft/core.clj
@@ -0,0 +1,35 @@
+(ns datacraft.core
+  (:use [org.httpkit.server :only [run-server]]
+        [mikera.cljutils error]
+        [clojure.repl])
+  (:require [clojure.tools.nrepl.server :as nrepl])
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [compojure.core :as cc :refer [GET POST DELETE PUT ANY OPTIONS defroutes]]
+            [ring.util.response :as resp]
+            [ring.middleware.cors :refer [wrap-cors]] 
+            [ring.middleware.params]
+            [clojure.java.jdbc :as j]
+            [cheshire.core :as json]
+            [clojure.string]
+            [clojure.java.io :as io])
+  (:require [clojure.tools.logging :as log])
+  (:require [org.httpkit.client :as http]))
+
+(defroutes app-routes
+  (route/resources "/")   ;; defaults to reading from /public path on classpath
+  (route/files "/" {:root "~/public"})
+  (route/files "/" {:root "/public"})
+  
+  (GET "/" [] (resp/redirect "/index.html"))
+
+  (route/not-found "<p>404 - Page Not Found.</p> 
+                    <p>Perhaps you want the <a href=\"\\index.html\">index.html</a>?</p>"))
+
+(def app
+  (-> #'app-routes 
+      ;; (wrap-cors :access-control-allow-origin #".*")
+      (handler/site)))
+
+(defn -main [& args] ;; entry point, lein run will enter from here
+  (run-server app {:port 80}))
