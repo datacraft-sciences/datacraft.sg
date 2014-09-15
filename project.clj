48b775787147dce0d32212f677b6dac9b65612bd
mikera <mike.r.anderson.13@gmail.com>
2014-09-15 09:55:24 +0800

    add project.clj

---
+++ project.clj
@@ -0,0 +1,46 @@
+(defproject datacraft "0.0.1-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[net.mikera/trellis "0.0.1"]
+                 [net.mikera/timeline "0.3.0"]
+                 [compojure "1.1.6"]
+                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]]
+
+  :plugins [[lein-cljsbuild "1.0.0"]
+            [lein-ring "0.8.7"]]
+
+  :ring {:handler datacraft.core/app
+         :init    datacraft.core/init}
+
+  :source-paths ["src/main/clj"]
+  :test-paths ["src/test/clj"]
+  
+  :resource-paths ["resources"]
+  
+  :main datacraft.core
+
+  :cljsbuild {
+              :builds [{:id "dev"
+                        :source-paths ["src/main/cljs"]
+                        :compiler {
+                                   :output-to "resources/public/js/app.js"
+                                   :output-dir "resources/public/js/out"
+                                   :optimizations :none
+                                   :source-map true
+                                   :externs ["om/externs/react.js"]}}
+                       {:id "release"
+                        :source-paths ["src/main/cljs"]
+                        :compiler {
+                                   :output-to "resources/public/js/app.js"
+                                   :source-map "resources/public/js/app.js.map"
+                                   :optimizations :advanced
+                                   :pretty-print false
+                                   :output-wrapper false
+                                   :preamble ["om/react.min.js"]
+                                   :externs ["om/externs/react.js"]
+                                   :closure-warnings
+                                   {:non-standard-jsdoc :off}}}]})
